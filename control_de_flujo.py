a167a2bf6a026cdd7a93aaeab357321ff3e86f00
hodgespaez <hodges10_24@hotmail.com>
2021-02-20 13:49:56 +0000

    ..

--- control_de_flujo.py
+++ control_de_flujo.py
@@ -55,8 +55,16 @@ está ordenada).
 lista1 = [12, 15, 20, 27, 32, 39, 42, 48, 55, 66, 75, 82, 89, 91, 93, 105, 123, 132, 150, 180, 201, 203, 231, 250, 260, 267, 300, 304, 310, 312, 321, 326]
 
 
-multiplos3= 17
+multiplos3= 17777
 print(multiplos3)
+
+multiplos3=[]
+
+for i  in lista1:
+  if i%3 == 0 and i<=300:
+    multiplos3.append(i)
+  i +=1
+print(len(multiplos3))
 
 
 """Guardar en `regresivo50` una lista con la cuenta regresiva desde el número 
@@ -97,21 +105,22 @@ print(invertido)
 Nota: Un número primo es un número entero que no se puede calcular multiplicando 
 otros números enteros.
 """
-numero= int(input("¿Qué número quieres saber si es primo? "))
-valor= range(2,numero)
-contador = 0
-for n in valor:
-  if numero % n == 0:
-    contador +=1
-    print("divisor:", n)
- 
-if contador > 0 :
-  print("El número no es primo" )
-else:
-  print(numero)    
+cont = 0
+primos = []
+for i in range(37, 300 + 1):
+    primos1 = True
+    for j in range(2,11):
+        if i == j:
+           break
+        elif i%j == 0:
+           primos1 = False
+        else:
+           continue
+    if primos1 == True:
+        primos.append(i)
+        cont += 1
+print(primos)
         
- 
-
 """Guardar en `fibonacci` una lista con los primeros 60 términos de la serie de 
 Fibonacci.
 Nota: En la serie de Fibonacci, los 2 primeros términos son 0 y 1, y a partir 
@@ -120,10 +129,16 @@ del segundo cada uno se calcula sumando los dos anteriores términos de la serie
 [0, 1, 1, 2, 3, 5, 8, ...]
 
 """
-
-
-
-
+fibonacci = []
+a = 0
+b = 1
+contando = 0
+while contando < 60:
+    fibonacci.append(a)
+    a, b = b, a+b
+    contando += 1
+    
+print(fibonacci)
 
 """Guardar en `factorial` el factorial de 30
 El factorial (símbolo:!) Significa multiplicar todos los números enteros desde
@@ -134,8 +149,17 @@ Por ejemplo, el factorial de 5 se calcula así:
 5! = 5 × 4 × 3 × 2 × 1 = 120
 """
 
+a = 30
 
+def calculaFactorial(n):
+  if n>0:
+    n = n * calculaFactorial(n - 1)
+  else:
+    n = 1
+  return n
 
+factorial = calculaFactorial(a)
+print (factorial)
 
 
 """Guarde en lista `pares` los elementos de la siguiente lista que esten 
@@ -144,10 +168,12 @@ presentes en posiciones pares, pero solo hasta la posición 80.
 
 lista3 = [941, 149, 672, 208, 99, 562, 749, 947, 251, 750, 889, 596, 836, 742, 512, 19, 674, 142, 272, 773, 859, 598, 898, 930, 119, 107, 798, 447, 348, 402, 33, 678, 460, 144, 168, 290, 929, 254, 233, 563, 48, 249, 890, 871, 484, 265, 831, 694, 366, 499, 271, 123, 870, 986, 449, 894, 347, 346, 519, 969, 242, 57, 985, 250, 490, 93, 999, 373, 355, 466, 416, 937, 214, 707, 834, 126, 698, 268, 217, 406, 334, 285, 429, 130, 393, 396, 936, 572, 688, 765, 404, 970, 159, 98, 545, 412, 629, 361, 70, 602]
 
-
-
-
-
+pares=[]
+for i, elemento in enumerate(lista3):
+  if i%2 ==0 and i<=80:
+    pares.append(elemento)
+  i +=1
+print(pares)
 
 """Guarde en lista `cubos` el cubo (potencia elevada a la 3) de los números del 
 1 al 100. 
@@ -161,7 +187,17 @@ lista3 = [941, 149, 672, 208, 99, 562, 749, 947, 251, 750, 889, 596, 836, 742, 5
 y guardar resultado en variable `suma_2s` 
 """
 
-
+n=1
+i=1
+serie = '2'
+suma_2s = 0
+while n <= 10:
+  while i < n:
+    serie += '2'
+    i+=1
+  suma_2s += int(serie)
+  n +=1
+print(suma_2s)
 
 
 
